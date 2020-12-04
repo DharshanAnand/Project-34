dcf46c96057e906f552254cc0c94c349b76e062a
DharshanAnand <69798018+DharshanAnand@users.noreply.github.com>
2020-12-04 14:06:14 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ var foodS;
 var foodStock;
 var dogimg;
 var doghimg;
+var bottle=20;
 
 function preload()
 {
@@ -19,26 +20,25 @@ function setup() {
   console.log(database);
 
   createCanvas(500, 500);
-  Dog= createSprite(250,250,10,10);
+  Dog = createSprite(250,250,10,10);
   //instead of Dog you wrote dog
   Dog.addImage(dogimg);
 Dog.scale = 0.2;
   foodStock=database.ref("food");
   foodStock.on("value",readStock);
 }
-
+ 
 
 function draw() {  
-background(46,139,87)
+background("yellow")
+textSize(20)
+text("food remaining:"+bottle,100,400);
   // you gave the drawsprites and text proeprty inside if statement.
   if(keyWentDown(UP_ARROW))
   {
    writeStock(foodS);
-  
-  
+   bottle=bottle-1
    Dog.addImage(doghimg);
-   
-  
   }
   drawSprites();
   text("Note:Press UP_ARROW Key To Feed Drake Milk",100,100)
